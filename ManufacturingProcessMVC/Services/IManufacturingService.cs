603a6958a37af15ba127817f675d8dc3774d96d8
mixail <v1tus5555@yandex.by>
2026-04-13 12:41:19 +0300

    In folder Services create interface IManufacturingService

---
+++ ManufacturingProcessMVC/Services/IManufacturingService.cs
@@ -0,0 +1,11 @@
+﻿using ManufacturingProcessMVC.Models;
+
+namespace ManufacturingProcessMVC.Services
+{
+    public interface IManufacturingService
+    {
+        Task<ManufacturingProcess> CreateProcessAsync(int drillId, int tapId);
+        Task ExecuteProcessAsync(int processId);
+        Task<IEnumerable<ManufacturingProcess>> GetAllProcessesAsync();
+    }
+}
