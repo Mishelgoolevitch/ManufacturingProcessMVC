2522d9c73ecde21fb21e4e0512419e9bf020ee67
mixail <v1tus5555@yandex.by>
2026-04-13 11:56:41 +0300

    In the Models folder, create Drill class and write the following

---
+++ ManufacturingProcessMVC/Models/Drill.cs
@@ -0,0 +1,19 @@
+﻿namespace ManufacturingProcessMVC.Models
+{
+    public class Drill : Instrument
+    {
+        public override string Type => "Drill";
+        public double Diameter { get; set; } // мм
+        public string? Material { get; set; }
+
+        public override void Prepare()
+        {
+            // Логика подготовки
+        }
+
+        public override void Execute()
+        {
+            // Логика выполнения
+        }
+    }
+}
