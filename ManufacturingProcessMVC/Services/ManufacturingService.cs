e89db4945fc7d0500f06083062f25c43b649e21e
mixail <v1tus5555@yandex.by>
2026-04-13 12:44:23 +0300

    In this folder add class ManufacturingService

---
+++ ManufacturingProcessMVC/Services/ManufacturingService.cs
@@ -0,0 +1,51 @@
+﻿using ManufacturingProcessMVC.Data;
+using ManufacturingProcessMVC.Data.Repositories;
+using ManufacturingProcessMVC.Models;
+
+namespace ManufacturingProcessMVC.Services
+{
+    public class ManufacturingService : IManufacturingService
+    {
+        private readonly IInstrumentRepository _instrumentRepository;
+        private readonly ApplicationDbContext _context;
+
+        public ManufacturingService(
+            IInstrumentRepository instrumentRepository,
+            ApplicationDbContext context)
+        {
+            _instrumentRepository = instrumentRepository;
+            _context = context;
+        }
+
+        public async Task<ManufacturingProcess> CreateProcessAsync(int drillId, int tapId)
+        {
+            var drill = await _instrumentRepository.GetDrillByIdAsync(drillId);
+            var tap = await _instrumentRepository.GetTapByIdAsync(tapId);
+
+            var process = new ManufacturingProcess
+            {
+                DrillId = drillId,
+                TapId = tapId,
+                Name = $"Process_{DateTime.Now:yyyyMMdd_HHmmss}",
+                CreatedDate = DateTime.Now,
+                Status = "Planned"
+            };
+
+            _context.Processes.Add(process);
+            await _context.SaveChangesAsync();
+
+            return process;
+        }
+
+        Task IManufacturingService.ExecuteProcessAsync(int processId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<ManufacturingProcess>> IManufacturingService.GetAllProcessesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
