7d7203721797dbf4e1959bbc02e91e0f250eebb4
mixail <v1tus5555@yandex.by>
2026-04-13 12:03:09 +0300

    In the Models folder, create the ManufacturingProcess class and write the following

---
+++ ManufacturingProcessMVC/Models/ManufacturingProcess.cs
@@ -0,0 +1,28 @@
+﻿namespace ManufacturingProcessMVC.Models
+{
+    public class ManufacturingProcess
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? Status { get; set; } // Planned, InProgress, Completed
+
+        // Навигационные свойства
+        public int? DrillId { get; set; }
+        public Drill? Drill { get; set; }
+
+        public int? TapId { get; set; }
+        public Tap? Tap { get; set; }
+
+        public void ExecuteProcess()
+        {
+            Drill?.Prepare();
+            Drill?.Execute();
+
+            Tap?.Prepare();
+            Tap?.Execute();
+
+            Status = "Completed";
+        }
+    }
+}
