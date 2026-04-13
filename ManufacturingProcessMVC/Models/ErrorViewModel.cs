c93212a2d9193305242808a07d8b3752611f5c88
mixail <v1tus5555@yandex.by>
2026-04-13 11:21:55 +0300

    Добавьте файлы проекта.

---
+++ ManufacturingProcessMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManufacturingProcessMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
