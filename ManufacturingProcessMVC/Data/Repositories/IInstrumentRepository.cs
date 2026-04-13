80203df644b87b81c804af760d9962c7ab3d6347
mixail <v1tus5555@yandex.by>
2026-04-13 12:29:21 +0300

    Create folder Repositories and write following interface

---
+++ ManufacturingProcessMVC/Data/Repositories/IInstrumentRepository.cs
@@ -0,0 +1,14 @@
+﻿using ManufacturingProcessMVC.Models;
+
+namespace ManufacturingProcessMVC.Data.Repositories
+{
+    public interface IInstrumentRepository
+    {
+        Task<IEnumerable<Drill>> GetAllDrillsAsync();
+        Task<IEnumerable<Tap>> GetAllTapsAsync();
+        Task<Drill> GetDrillByIdAsync(int id);
+        Task<Tap> GetTapByIdAsync(int id);
+        Task AddDrillAsync(Drill drill);
+        Task AddTapAsync(Tap tap);
+    }
+}
