49212f7d0efb01d2a5206b4ed4175282e3444eb4
mixail <v1tus5555@yandex.by>
2026-04-13 12:10:17 +0300

    Create folder Data and write following class ApplicationDbContext.cs

---
+++ ManufacturingProcessMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ManufacturingProcessMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManufacturingProcessMVC.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Drill> Drills { get; set; }
+        public DbSet<Tap> Taps { get; set; }
+        public DbSet<ManufacturingProcess> Processes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+                 modelBuilder.Entity<Instrument>()
+                .HasDiscriminator<string>("InstrumentType")
+                .HasValue<Drill>("Drill")
+                .HasValue<Tap>("Tap");
+        }
+    }
+}
