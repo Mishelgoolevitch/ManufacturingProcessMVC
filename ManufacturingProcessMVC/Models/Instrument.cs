333f848ab8d43f0c1a0c5e153b2ddd992381c076
mixail <v1tus5555@yandex.by>
2026-04-13 11:49:12 +0300

    In the Models folder, create the Instrument class and write the following

---
+++ ManufacturingProcessMVC/Models/Instrument.cs
@@ -0,0 +1,12 @@
+﻿namespace ManufacturingProcessMVC.Models
+{
+    public abstract class Instrument
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public abstract string Type { get; }
+
+        public abstract void Prepare();
+        public abstract void Execute();
+    }
+}
