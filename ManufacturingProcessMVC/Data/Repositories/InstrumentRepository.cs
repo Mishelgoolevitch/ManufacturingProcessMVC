db45f1b271e49482a91603520274d7390cea0aef
mixail <v1tus5555@yandex.by>
2026-04-13 12:38:08 +0300

    In the Repositories folder , creat and write following

---
+++ ManufacturingProcessMVC/Data/Repositories/InstrumentRepository.cs
@@ -0,0 +1,45 @@
+﻿using ManufacturingProcessMVC.Models;
+
+namespace ManufacturingProcessMVC.Data.Repositories
+{
+    public class InstrumentRepository: IInstrumentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InstrumentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddDrillAsync(Drill drill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddTapAsync(Tap tap)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Drill>> GetAllDrillsAsync()
+        {
+            throw new NotImplementedException();
+            //return await _context.Drills.ToListAsync();
+        }
+
+        public Task<IEnumerable<Tap>> GetAllTapsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Drill> GetDrillByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tap> GetTapByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
