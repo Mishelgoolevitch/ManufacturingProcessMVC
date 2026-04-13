25f67ef45fe726a5ac7fe6f40826b02ac004c598
mixail <v1tus5555@yandex.by>
2026-04-13 11:59:35 +0300

    In the Models folder, create the Tap class and write the following

---
+++ ManufacturingProcessMVC/Models/Tap.cs
@@ -0,0 +1,19 @@
+﻿namespace ManufacturingProcessMVC.Models
+{
+    public class Tap : Instrument
+    {
+        public override string Type => "Tap";
+        public string? Size { get; set; } // М10, М12 и т.д.
+        public string? ThreadType { get; set; }
+
+        public override void Prepare()
+        {
+            // Логика подготовки
+        }
+
+        public override void Execute()
+        {
+            // Логика выполнения
+        }
+    }
+}
